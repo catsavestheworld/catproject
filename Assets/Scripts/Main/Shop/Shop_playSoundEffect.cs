283d5ad8274b3c26db9e61347bc45ed2033c3c72
mozzi0131 <sue03094@gmail.com>
2020-04-25 01:03:55 +0900

    add Asset's meta file

---
+++ Assets/Scripts/Main/Shop/Shop_playSoundEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop_playSoundEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject AudioManager;
+    Vector3 volVector;
+    float effectvolume;
+    AudioClip catEffect;
+    void Start()
+    {
+        AudioManager = GameObject.Find("AudioManager");
+    }
+
+    void getSetting(){
+        volVector = AudioManager.GetComponent<Main_AudioManager>().effectVector;
+        effectvolume = AudioManager.GetComponent<Main_AudioManager>().effectVol;
+        catEffect = AudioManager.GetComponent<Main_AudioManager>().Cat_Meow[0];
+    }
+
+    // Update is called once per frame
+    private void OnMouseDown() {
+        if(catEffect == null){
+            getSetting();
+
+        }
+        
+        if (effectvolume != 0){
+            AudioSource.PlayClipAtPoint(catEffect, volVector);
+        }
+    }
+}
