e278e75925e808b9b06dc8565af60ab324d50a30
ji-water <bjisoo72@gmail.com>
2020-04-30 13:20:20 +0900

    add untracked file

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public void pauseTime()
+    {
+        Time.timeScale = 0;
+    }
+    public void resumeTime()
+    {
+        Time.timeScale = 1;
+    }
+}
